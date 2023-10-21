3bc8fa0eec48dfa7886731300eff2a5979951579
Brandon <brandonwalsh820@gmail.com>
2023-10-21 09:52:14 -0400

    Initial Commit

---
+++ src/main/java/net/brndnwlsh/explorationmotivation/ExplorationMotivationClient.java
@@ -0,0 +1,10 @@
+package net.brndnwlsh.explorationmotivation;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExplorationMotivationClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/brndnwlsh/explorationmotivation/ExplorationMotivationDataGenerator.java
@@ -0,0 +1,11 @@
+package net.brndnwlsh.explorationmotivation;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ExplorationMotivationDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
